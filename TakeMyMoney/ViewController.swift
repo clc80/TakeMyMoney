df64b352fac594fe069e7d5ee72c2a8f17559eb7
Claudia Maciel <clc_80@yahoo.com>
2021-02-09 17:31:01 -0800

    Initial Commit

---
+++ TakeMyMoney/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TakeMyMoney
+//
+//  Created by Claudia Maciel on 2/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
