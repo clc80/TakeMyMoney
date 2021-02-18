70d32759fb7fd9518af1c32fed4634d0daf70ba0
Claudia Maciel <clc_80@yahoo.com>
2021-02-18 12:42:23 -0800

    UI for confirmation screen completed

---
+++ TakeMyMoney/Controller/PaymentConfirmationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PaymentConfirmationViewController.swift
+//  TakeMyMoney
+//
+//  Created by Claudia Maciel on 2/18/21.
+//
+
+import UIKit
+
+class PaymentConfirmationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
